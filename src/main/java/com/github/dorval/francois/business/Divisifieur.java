fc2e91212185600c7a657122ddd06d8d3a67b3c7
Vinsouille <abgrall.vincent29@gmail.com>
2020-07-10 23:23:02 +0200

    Workflows 5

--- src/main/java/com/github/dorval/francois/business/Divisifieur.java
+++ src/main/java/com/github/dorval/francois/business/Divisifieur.java
@@ -10,10 +10,6 @@ public class Divisifieur {
      */
     public static int diviser(int dividende, int diviseur)
     {
-        if (diviseur == 0){
-            throw new IllegalArgumentException(Errors.PAS_DE_DIV_PAR_0);
-        }
-
         return dividende/diviseur;
     }
 }
